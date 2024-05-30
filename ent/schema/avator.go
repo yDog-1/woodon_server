cf896382a7c0334afc22de7bb144f188c0646bf7
yDog <ydog-1@outlook.jp>
2024-05-30 11:59:56 +0000

    userとavatorのスキーマを定義

--- ent/schema/avator.go
+++ ent/schema/avator.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Avator holds the schema definition for the Avator entity.
 type Avator struct {
@@ -9,10 +13,18 @@ type Avator struct {
 
 // Fields of the Avator.
 func (Avator) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name").
+			NotEmpty(),
+		field.String("bio"),
+	}
 }
 
 // Edges of the Avator.
 func (Avator) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("owner", User.Type).
+			Ref("avators").
+			Unique(),
+	}
 }
